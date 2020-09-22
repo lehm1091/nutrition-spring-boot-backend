3bac33d411933fbf5c20a01780929afd8faee74f
Luis H D <lehm1091@gmail.com>
2020-09-22 15:57:01 -0600

    added imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
-insert into food_categories(id,name) values (1,"protein");
-insert into food_categories(id,name) values (1,"meat");
+insert into food_categories(id,name) values (1,"Protein");
+insert into food_categories(id,name) values (2,"Vegetables ");
+insert into food_categories(id,name) values (3,"Breads");
+insert into food_categories(id,name) values (4,"Milk");
+insert into food_categories(id,name) values (5,"Fish");
+insert into food_categories(id,name) values (6,"Cereals");
+insert into food_categories(id,name) values (7,"Pasta");

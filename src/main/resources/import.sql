a5137c217117cc51fb6ac67f040df88c232ddc8f
Luis H D <lehm1091@gmail.com>
2020-09-23 19:45:55 -0600

    create drop

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,10 @@
 insert into food_categories(id,name) values (1,"Protein");
-insert into food_categories(id,name) values (2,"Vegetables ");
+insert into food_categories(id,name) values (2,"Vegetables");
 insert into food_categories(id,name) values (3,"Breads");
 insert into food_categories(id,name) values (4,"Milk");
 insert into food_categories(id,name) values (5,"Fish");
 insert into food_categories(id,name) values (6,"Cereals");
 insert into food_categories(id,name) values (7,"Pasta");
+insert into food_categories(id,name) values (8,"Other");
+
+
